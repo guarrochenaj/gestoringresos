198661433ad5abb48477a699e5813ec8b5615fff
Franco <francoalvarez@live.com>
2021-11-03 20:10:52 -0300

    funcionando

--- GIE2.0.py
+++ GIE2.0.py
@@ -40,38 +40,160 @@ class Aplicacion(tk.Frame):
         boton.pack(padx="10", pady="20")
         boton.place(x=270, y=370)
 
-        numeroPantalla = StringVar()
 
-        pantalla = Entry(tab_datos, textvariable=numeroPantalla, bg="grey", fg="black")
-        pantalla.place(x=100, y=100)
 
-        cuadroTexto = Entry(tab_datos)
-        cuadroTexto.place(x=100, y=150)
+        '''cuadroTexto = Entry(tab_datos)
+        cuadroTexto.place(x=100, y=150)'''
 
         style = ttk.Style()
-        style.theme_use("default")
+        style.theme_use("clam")
         style.configure("block.Horizontal.TProgressbar", background="green")
         bar = Progressbar(tab_datos, length=200, style="block.Horizontal.TProgressbar")
-        bar["value"] = 95
+        bar["value"] = 100
         bar.place(x=60, y=200)
 
         tab_ingreso = ttk.Frame(contenedor)
         contenedor.add(tab_ingreso, text='Ingreso')
 
-        lbl_ingreso = tk.Label(tab_ingreso, text='Ingreso')
-        lbl_ingreso.grid(row=0, column=0)
+        '''lbl_ingreso = tk.Label(tab_ingreso, text='Ingreso')
+        lbl_ingreso.grid(row=0, column=0)'''
 
-        txt_nombre = tk.Entry(tab_ingreso)
-        txt_nombre.grid(row=1, column=0)
+        '''txt_nombre = tk.Entry(tab_ingreso)
+        txt_nombre.grid(row=1, column=0)'''
+
+        texto_pantalla = StringVar()
+
+        pantalla = Entry(tab_datos, textvariable=texto_pantalla, bg="grey", fg="black")
+        pantalla.place(x=100, y=100)
+
+        color_boton = "grey80"
+        ancho_boton = 6
+        alto_boton = 3
+
+        operador0 = ""
+        operador1 = ""
+
+        texto_pantalla0 = StringVar()
+        texto_pantalla1 = StringVar()
+
+        
+
+        def clear0():
+            global operador0
+            operador0 = ""
+            texto_pantalla0.set("0")
+
+        def click0(b):
+            global operador0
+            operador0 += str(b)
+            texto_pantalla0.set(operador0)
+
+        def resultado0():
+            global operador0
+            try:
+                r = str(eval(operador0))
+            except:
+                r = "ERROR"
+            texto_pantalla0.set(r)
+
+        def clear1():
+            global operador1
+            operador1 = ""
+            texto_pantalla1.set("0")
+
+        def click1(b):
+            global operador1
+            operador1 += str(b)
+            texto_pantalla1.set(operador1)
+
+        def resultado1():
+            global operador1
+            try:
+                r = str(eval(operador1))
+            except:
+                r = "ERROR"
+            texto_pantalla1.set(r)
+
+#ingreso
+        clear0()
+        clear1()
+        Boton0 = Button(tab_ingreso, text="0", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(0)).grid(row=5, column=0, pady=10)
+        Boton1 = Button(tab_ingreso, text="1", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(1)).grid(row=4, column=0, pady=10)
+        Boton2 = Button(tab_ingreso, text="2", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(2)).grid(row=4, column=1, pady=10)
+        Boton3 = Button(tab_ingreso, text="3", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(3)).grid(row=4, column=2, pady=10)
+        Boton4 = Button(tab_ingreso, text="4", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(4)).grid(row=3, column=0, pady=10)
+        Boton5 = Button(tab_ingreso, text="5", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(5)).grid(row=3, column=1, pady=10)
+        Boton6 = Button(tab_ingreso, text="6", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(6)).grid(row=3, column=2, pady=10)
+        Boton7 = Button(tab_ingreso, text="7", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(7)).grid(row=2, column=0, pady=10)
+        Boton8 = Button(tab_ingreso, text="8", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(8)).grid(row=2, column=1, pady=10)
+        Boton9 = Button(tab_ingreso, text="9", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click0(9)).grid(row=2, column=2, pady=10)
+        '''BotonSuma = Button(tab_ingreso, text="+", bg=color_boton, width=ancho_boton,
+                           height=alto_boton, command=lambda: click("+")).grid(row=6, column=0, pady=10)
+        BotonResta = Button(tab_ingreso, text="-", bg=color_boton, width=ancho_boton,
+                            height=alto_boton, command=lambda: click("-")).grid(row=6, column=1, pady=10)'''
+        BotonClear = Button(tab_ingreso, text="Clear", bg=color_boton, width=5,
+                            height=1, command=clear0).grid(row=1, column=0, pady=10)
+        BotonIgual = Button(tab_ingreso, text="=", bg=color_boton, width=ancho_boton,
+                            height=alto_boton, command=resultado0).grid(row=5, column=2, pady=10)
+
+        Pantalla = Entry(tab_ingreso, font=("arial", 20, "bold"), width=20,
+                         borderwidth=10, background="green", textvariable=texto_pantalla0)
+        Pantalla.grid(row=0, column=0, columnspan=3, padx=0, pady=0)
+
+#egreso
 
         tab_egreso = ttk.Frame(contenedor)
         contenedor.add(tab_egreso, text='Egreso')
 
-        lbl_egreso = tk.Label(tab_egreso, text='Egreso')
+        '''lbl_egreso = tk.Label(tab_egreso, text='Egreso')
         lbl_egreso.grid(row=0, column=0)
 
         txt_nombre = tk.Entry(tab_egreso)
-        txt_nombre.grid(row=1, column=0)
+        txt_nombre.grid(row=1, column=0)'''
+
+        Boton0 = Button(tab_egreso, text="0", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(0)).grid(row=5, column=0, pady=10)
+        Boton1 = Button(tab_egreso, text="1", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(1)).grid(row=4, column=0, pady=10)
+        Boton2 = Button(tab_egreso, text="2", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(2)).grid(row=4, column=1, pady=10)
+        Boton3 = Button(tab_egreso, text="3", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(3)).grid(row=4, column=2, pady=10)
+        Boton4 = Button(tab_egreso, text="4", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(4)).grid(row=3, column=0, pady=10)
+        Boton5 = Button(tab_egreso, text="5", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(5)).grid(row=3, column=1, pady=10)
+        Boton6 = Button(tab_egreso, text="6", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(6)).grid(row=3, column=2, pady=10)
+        Boton7 = Button(tab_egreso, text="7", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(7)).grid(row=2, column=0, pady=10)
+        Boton8 = Button(tab_egreso, text="8", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(8)).grid(row=2, column=1, pady=10)
+        Boton9 = Button(tab_egreso, text="9", bg=color_boton, width=ancho_boton,
+                        height=alto_boton, command=lambda: click1(9)).grid(row=2, column=2, pady=10)
+        '''BotonSuma = Button(tab_egreso, text="+", bg=color_boton, width=ancho_boton,
+                           height=alto_boton, command=lambda: click1("+")).grid(row=6, column=0, pady=10)
+        BotonResta = Button(tab_egreso, text="-", bg=color_boton, width=ancho_boton,
+                            height=alto_boton, command=lambda: click1("-")).grid(row=6, column=1, pady=10)'''
+        BotonClear = Button(tab_egreso, text="Clear", bg=color_boton, width=5,
+                            height=1, command=clear1).grid(row=1, column=0, pady=10)
+        BotonIgual = Button(tab_egreso, text="=", bg=color_boton, width=ancho_boton,
+                            height=alto_boton, command=resultado1).grid(row=5, column=2, pady=10)
+
+        Pantalla = Entry(tab_egreso, font=("arial", 20, "bold"), width=20,
+                         borderwidth=10, background="red", textvariable=texto_pantalla1)
+        Pantalla.grid(row=0, column=0, columnspan=3, padx=0, pady=0)
+
 
         contenedor.pack(expand=1, fill='both')
 
--- backupGIE.py
+++ backupGIE.py
@@ -37,40 +37,40 @@ def resultado():
 
 clear()
 # BOTONES DE LA PRIMERA FILA
-Boton0 = Button(ventana, text="0", bg=color_boton, width=ancho_boton,
+Boton0 = Button(tab_ingreso, text="0", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(0)).grid(row=5, column=0, pady=10)
-Boton1 = Button(ventana, text="1", bg=color_boton, width=ancho_boton,
+Boton1 = Button(tab_ingreso, text="1", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(1)).grid(row=4, column=0, pady=10)
-Boton2 = Button(ventana, text="2", bg=color_boton, width=ancho_boton,
+Boton2 = Button(tab_ingreso, text="2", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(2)).grid(row=4, column=1, pady=10)
-Boton3 = Button(ventana, text="3", bg=color_boton, width=ancho_boton,
+Boton3 = Button(tab_ingreso, text="3", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(3)).grid(row=4, column=2, pady=10)
 # BOTONES DE LA SEGUNDA FILA
-Boton4 = Button(ventana, text="4", bg=color_boton, width=ancho_boton,
+Boton4 = Button(tab_ingreso, text="4", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(4)).grid(row=3, column=0, pady=10)
-Boton5 = Button(ventana, text="5", bg=color_boton, width=ancho_boton,
+Boton5 = Button(tab_ingreso, text="5", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(5)).grid(row=3, column=1, pady=10)
-Boton6 = Button(ventana, text="6", bg=color_boton, width=ancho_boton,
+Boton6 = Button(tab_ingreso, text="6", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(6)).grid(row=3, column=2, pady=10)
-Boton7 = Button(ventana, text="7", bg=color_boton, width=ancho_boton,
+Boton7 = Button(tab_ingreso, text="7", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(7)).grid(row=2, column=0, pady=10)
 # BOTONES DE LA TERCERA FILA
-Boton8 = Button(ventana, text="8", bg=color_boton, width=ancho_boton,
+Boton8 = Button(tab_ingreso, text="8", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(8)).grid(row=2, column=1, pady=10)
-Boton9 = Button(ventana, text="9", bg=color_boton, width=ancho_boton,
+Boton9 = Button(tab_ingreso, text="9", bg=color_boton, width=ancho_boton,
                 height=alto_boton, command=lambda: click(9)).grid(row=2, column=2, pady=10)
 # BOTONES DE LA CUARTA FILA
-BotonSuma = Button(ventana, text="+", bg=color_boton, width=ancho_boton,
+BotonSuma = Button(tab_ingreso, text="+", bg=color_boton, width=ancho_boton,
                    height=alto_boton, command=lambda: click("+")).grid(row=6, column=0, pady=10)
-BotonResta = Button(ventana, text="-", bg=color_boton, width=ancho_boton,
+BotonResta = Button(tab_ingreso, text="-", bg=color_boton, width=ancho_boton,
                     height=alto_boton, command=lambda: click("-")).grid(row=6, column=1, pady=10)
 # BOTONES DE LA QUINTA FILA
-BotonClear = Button(ventana, text="Clear", bg=color_boton, width=ancho_boton,
+BotonClear = Button(tab_ingreso, text="Clear", bg=color_boton, width=ancho_boton,
                     height=alto_boton, command=clear).grid(row=1, column=0, pady=10)
-BotonIgual = Button(ventana, text="=", bg=color_boton, width=ancho_boton,
+BotonIgual = Button(tab_ingreso, text="=", bg=color_boton, width=ancho_boton,
                     height=alto_boton, command=resultado).grid(row=5, column=1, pady=10)
 
-Pantalla = Entry(ventana, font=("arial", 20, "bold"), width=22,
+Pantalla = Entry(tab_ingreso, font=("arial", 20, "bold"), width=22,
                  borderwidth=10, background="CadetBlue1", textvariable=texto_pantalla)
 Pantalla.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
 
